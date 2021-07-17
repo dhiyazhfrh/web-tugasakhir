da418174bf6c62e0d0966e0d0164af76e6194b15
dhiyazhfrh <dhiyazhafirah86@gmail.com>
2021-07-17 13:44:12 +0700

    first commit

---
+++ Home.php
@@ -0,0 +1,46 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Grow</title>
+        <link rel="stylesheet" type="text/css" href="Style.css">
+        <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet"> 
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>
+    <body>
+        <header>
+            <img class="logo" src="img/logo1.png">
+            <nav>
+                <ul class="nav__links">
+                    <li><a href="#">Our Garden</a></li>
+                    <li><a href="#">Contact Us</a></li>
+                    <li><a href="#">Login</a></li>
+                </ul>
+            </nav>
+        </header>
+        <div class="container1">
+            <img class="photo" src="img/frame1.png">
+        </div>
+        <div class="container2">
+            <div class="row1">
+                <img class="photo" src="img/mock.png">
+                <div class="text1">
+                    <h2>Awesome Application</h2>
+                    <a>Focus on develop our garden with awesome IoT products. We created mobile and website application.</a>
+                </div>
+            </div>
+        </div>
+        <div class="container2">
+            <div class="row2">
+                <div class="text2">
+                    <h2>Cloud Platform</h2>
+                    <a>Real data was an important things. We created this application using Realtime Database.</a>
+                </div>
+                <img class="photo" src="img/cloud.svg">
+            </div>
+        </div>
+        <div class="footer">
+            <p id="pfooter">©Copyright 2021</p>
+        </div>
+    </body>
+</html>
