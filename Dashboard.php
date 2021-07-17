19fdabf457f6808dc99caeae385b45509b23ca6f
dhiyazhfrh <dhiyazhafirah86@gmail.com>
2021-07-17 19:55:13 +0700

    update dashboard

---
+++ Dashboard.php
@@ -0,0 +1,51 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Grow Application</title>
+        <link rel="stylesheet" type="text/css" href="Style.css">
+        <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet"> 
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>
+    <body>
+        <div class="sidebar">
+            <div class="sidebar-brand">
+            <img class="logo2" src="img/logo2.png">
+            </div>
+            <div class="sidebar-menu">
+                <ul>
+                    <li><a href="#"><img class="icon" src="img/dash.svg">Dashboard</a></li>
+                    <li><a href="#"><img class="icon" src="img/statik.svg">Statistics</a></li>
+                    <li><a href="#"><img class="icon" src="img/component.svg">Components</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="main-dash">
+            <header>
+                <h1><label for=""><span class="las la-bars"></span></label> Our Garden</h1>
+            </header>
+            <main>
+                <div class="cards">
+                    <div class="card-single">
+                        <div>
+                            <h1>0</h1>
+                            <span>Moisture</span>
+                        </div>
+                        <div>
+                            <span class="icon2" src="img/mois.svg"></span>
+                        </div>
+                    </div>
+                    <div class="card-single">
+                        <div>
+                            <h1>0</h1>
+                            <span>Moisture</span>
+                        </div>
+                        <div>
+                            <span class="icon2" src="img/mois.svg"></span>
+                        </div>
+                    </div>
+                </div>
+            </main>
+        </div>
+    </body>
+</html> 
