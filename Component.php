af1144ac1f9cba297adc95fa9126c51c3d01d227
surya-rifqi <suryarifqi21@gmail.com>
2021-07-20 20:47:33 +0700

    saya menyerah pada chart

--- Component.php
+++ Component.php
@@ -17,8 +17,6 @@
                 <ul>
                     <li><a class="list" href="Dashboard.php"><span class="las la-igloo"></span>
                         <span>Dashboard</span></a></li>
-                    <li><a class="list" href="Statistic.php"><span class="las la-chart-line"></span>
-                        <span>Statistics</span></a></li>
                     <li><a class="list active" href="Component.php"><span class="las la-microchip"></span>
                         <span>Components</span></a></li>
                     <li><a class="list" href="Contact.php"><span class="las la-user-friends"></span>
