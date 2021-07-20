7ed7bc919177035f60f031d473fe0aab4d989419
surya-rifqi <suryarifqi21@gmail.com>
2021-07-20 18:24:30 +0700

    .

--- Chart.js
+++ Chart.js
@@ -1,4 +1,5 @@
 var temperature = [], humidity = [], water_level = [], moisture = [];
+ref = firebase.database().ref('sensor');
 
 ref.on('value', , function(childSnapshot){
     for (let i in childSnapshot.val().humidity){
@@ -14,41 +15,75 @@ function drawGraph(humidity){
     var ctx = document.getElementById("myChart").getContext('2d');
     var myChart = new CharacterData(ctx, {
         type: 'line',
-        data : {
-                label: "humidity [%RH]",
-                labelString : "hum",
-
-                borderColor: 'rgb(0, 99, 132)',
-                backgroundColor: 'rgb(0, 99, 132)',
-                fill: false,
-                data: hum,
-                yAxisID: "y-axis-temp", 
-
-            },
-
-            options: {
-                responsive: true,
-                maintainAspectRatio: false,
-                hoverMode: 'index',
-                stacked: false,
-                title:{
-                    display: true,
-                    text:'Last 17 sensor measurements'
-                },
-
-                scales: {
-                    yAxes: [{
-                        type: 'linear',
-                        display: true,
-                        position: "left",
-                        id: "y-axis-temp",
-                        ticks: {
-                            beginAtZero: true,
-                            siggestedMax: 50
-                        }
-                    }],
-                }
-            }
+        data: {
+          labels: labels,
+          datasets: [{
+          label: "temperature [°C]",
+          labelString : "°C",
+          borderColor: 'rgb(255, 99, 132)',
+          backgroundColor: 'rgb(255, 99, 132)',
+          fill: false,
+          data: temp,
+            yAxisID: "y-axis-temp",
+          },
+           {
+                  label: "humidity [%RH]",
+                 labelString : "hum",
+      
+          borderColor: 'rgb(0, 99, 132)',
+          backgroundColor: 'rgb(0, 99, 132)',
+          fill: false,
+          data: hum,
+            yAxisID: "y-axis-temp",         
+                     
+        },
+                       {
+                  label: "light [%]",
+                 labelString : "light",
+      
+          borderColor: 'rgb(145, 70, 65)',
+          backgroundColor: 'rgb(145, 70, 65)',
+          fill: false,
+          data: light,
+            yAxisID: "y-axis-temp",         
+                     
+        },
+                       {
+                  label: "motion [0-1]",
+                 labelString : "motion",
+      
+          borderColor: 'rgb(0, 21, 32)',
+          backgroundColor: 'rgb(0, 21, 32)',
+          fill: false,
+          data: motion,
+            yAxisID: "y-axis-temp",         
+                     
+        }]
+        },
+                  options: {
+                      responsive: true,
+                    maintainAspectRatio: false,
+                    hoverMode: 'index',
+                      stacked: false,
+                      title:{
+                          display: true,
+                          text:'Last 20 room measurements'
+                      },
+      
+                      scales: {
+                          yAxes: [{
+                              type: "linear", 
+                              display: true,
+                              position: "left",
+                              id: "y-axis-temp",
+                              ticks: {
+                                beginAtZero:true,
+                                suggestedMax: 50
+                              }
+                    
+                          }],
+                      }
+                  }
         
     });
     }
